48e55ffcdb571108757d8080b601861f8a2524b4
Barthm07 <146319591+Barthm07@users.noreply.github.com>
2026-04-20 08:29:26 +0200

    ConnectionFix
    User can now enter the server ip, and it connects to the internet ip making sure its the same networl

--- obj/Debug/net10.0-windows/ManagerComputer_15rit502_wpftmp.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/ManagerComputer_15rit502_wpftmp.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManagerComputer")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dd2a3ac93fcb4ec27d8233a7ff7498044ca1239")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManagerComputer")]
